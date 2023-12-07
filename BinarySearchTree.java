055d1becba256e62f4721889de8b56270dffff50
Jemal Mohamedngus <jemalmngus@gmail.com>
2023-12-07 22:19:13 +0300

    add binary search tree

---
+++ BinarySearchTree.java
@@ -0,0 +1,136 @@
+class Node {
+    int key;
+    Node left, right;
+
+    public Node(int item) {
+        key = item;
+        left = right = null;
+    }
+}
+
+public class BinarySearchTree {
+    private Node root;
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    // Insert a key into the BST
+    public void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    // A recursive function to insert a new key into the BST
+    private Node insertRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        if (key < root.key) {
+            root.left = insertRec(root.left, key);
+        } else if (key > root.key) {
+            root.right = insertRec(root.right, key);
+        }
+
+        return root;
+    }
+
+    // Delete a key from the BST
+    public void delete(int key) {
+        root = deleteRec(root, key);
+    }
+
+    // A recursive function to delete a key from the BST
+    private Node deleteRec(Node root, int key) {
+        if (root == null) {
+            return root;
+        }
+
+        if (key < root.key) {
+            root.left = deleteRec(root.left, key);
+        } else if (key > root.key) {
+            root.right = deleteRec(root.right, key);
+        } else {
+            // Node with only one child or no child
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            // Node with two children, get the inorder successor (smallest
+            // in the right subtree)
+            root.key = minValue(root.right);
+
+            // Delete the inorder successor
+            root.right = deleteRec(root.right, root.key);
+        }
+
+        return root;
+    }
+
+    private int minValue(Node root) {
+        int minValue = root.key;
+        while (root.left != null) {
+            minValue = root.left.key;
+            root = root.left;
+        }
+        return minValue;
+    }
+
+    // Search for a key in the BST
+    public boolean search(int key) {
+        return searchRec(root, key);
+    }
+
+    // A recursive function to search for a key in the BST
+    private boolean searchRec(Node root, int key) {
+        if (root == null || root.key == key) {
+            return root != null;
+        }
+
+        if (key < root.key) {
+            return searchRec(root.left, key);
+        } else {
+            return searchRec(root.right, key);
+        }
+    }
+
+    // In-order traversal of the BST
+    public void inOrder() {
+        inOrderRec(root);
+    }
+
+    private void inOrderRec(Node root) {
+        if (root != null) {
+            inOrderRec(root.left);
+            System.out.print(root.key + " ");
+            inOrderRec(root.right);
+        }
+    }
+
+    // Example usage
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+
+        System.out.println("In-order traversal of the BST:");
+        tree.inOrder();
+
+        int keyToSearch = 40;
+        System.out.println("\nSearch for key " + keyToSearch + ": " + tree.search(keyToSearch));
+
+        int keyToDelete = 30;
+        tree.delete(keyToDelete);
+        System.out.println("\nIn-order traversal after deleting key " + keyToDelete + ":");
+        tree.inOrder();
+    }
+}
